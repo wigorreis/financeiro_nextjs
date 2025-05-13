dbf7c2c200bd56b2dbf7bd43b8361d36e1f9ebe1
Wigor Paulo <wigorpaulo@gmail.com>
2025-05-12 21:50:09 -0300

    first commit

---
+++ financeiro/src/app/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@mui/material";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Novo sistema surgindo!</p>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+}
